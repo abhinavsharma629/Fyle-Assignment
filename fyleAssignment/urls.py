0704f6c8203a7799584efc437cf54950e30f0125
Abhinav Sharma <abhinavsharma629@gmail.com>
2019-07-22 19:43:22 +0530

    V 1.1.1

--- fyleAssignment/urls.py
+++ fyleAssignment/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('', include('mainApi.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'), #For future use to verify if an access token is valid or not
 ]
